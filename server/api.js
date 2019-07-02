0dc42ec775ead0ecafe74761afb7361dbb6511f8
nik frank <nikbfrank@gmail.com>
2019-07-02 06:26:32 -0700

    refactor routes into api file

---
+++ server/api.js
@@ -0,0 +1,40 @@
+module.exports = (app, { User, Meme, Vote })=>{
+  app.post('/user', (req, res)=>{
+    User.create(req.body)
+        .then(()=> res.json({ message: 'user created' }))
+        .catch(err => {
+          res.status(500).json({ message: JSON.stringify(err) })
+        });
+  });
+
+  app.get('/user/:id', (req, res)=>{
+    User.findByPk(1*req.params.id)
+      .then(user => res.json(user));
+  });
+
+  app.post('/meme', (req, res)=>{
+    Meme.create(req.body)
+        .then(()=> res.json({ message: 'meme created' }))
+        .catch(err => {
+          res.status(500).json({ message: JSON.stringify(err) })
+        });
+  });
+
+  app.get('/meme/:id', (req, res)=>{
+    Meme.findByPk(1*req.params.id)
+      .then(meme => res.json(meme));
+  });
+
+  app.post('/vote', (req, res)=>{
+    Vote.create(req.body)
+        .then(()=> res.json({ message: 'vote created' }))
+        .catch(err => {
+          res.status(500).json({ message: JSON.stringify(err) })
+        });
+  });
+
+  app.get('/vote/:id', (req, res)=>{
+    Vote.findByPk(1*req.params.id)
+      .then(vote => res.json(vote));
+  });
+}
--- server/index.js
+++ server/index.js
@@ -8,6 +8,8 @@ const connection = new ORM('postgres://meme_wars:guest@localhost:5432/meme_wars'
 const modelsFactory = require('./models');
 const { User, Meme, Vote } = modelsFactory(connection, ORM);
 
+const api = require('./api')(app, { User, Meme, Vote });
+
 connection.authenticate()
   .then(()=> console.log('success'))
   .catch((err)=> console.log(err));
@@ -23,46 +25,5 @@ app.get('/hydrate', (req, res)=> {
     .then(()=> res.json({ message: 'success creating User, Meme, Vote tables' }))
     .catch(err => res.status(500).json({ message: JSON.stringify(err) }));
 });
-
-app.post('/user', (req, res)=>{
-  User.create(req.body)
-      .then(()=> res.json({ message: 'user created' }))
-      .catch(err => {
-        res.status(500).json({ message: JSON.stringify(err) })
-      });
-});
-
-app.get('/user/:id', (req, res)=>{
-  User.findByPk(1*req.params.id)
-    .then(user => res.json(user));
-});
-
-app.post('/meme', (req, res)=>{
-  Meme.create(req.body)
-      .then(()=> res.json({ message: 'meme created' }))
-      .catch(err => {
-        res.status(500).json({ message: JSON.stringify(err) })
-      });
-});
-
-app.get('/meme/:id', (req, res)=>{
-  Meme.findByPk(1*req.params.id)
-    .then(meme => res.json(meme));
-});
-
-app.post('/vote', (req, res)=>{
-  Vote.create(req.body)
-      .then(()=> res.json({ message: 'vote created' }))
-      .catch(err => {
-        res.status(500).json({ message: JSON.stringify(err) })
-      });
-});
-
-app.get('/vote/:id', (req, res)=>{
-  Vote.findByPk(1*req.params.id)
-    .then(vote => res.json(vote));
-});
-
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
