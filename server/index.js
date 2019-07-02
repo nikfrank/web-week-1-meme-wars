44cf5c976199fe29461a4b1ec804a44c0a664237
nik frank <nikbfrank@gmail.com>
2019-07-02 06:04:27 -0700

    routes for create, get by id for meme, vote

--- server/index.js
+++ server/index.js
@@ -26,22 +26,43 @@ app.get('/hydrate', (req, res)=> {
 
 app.post('/user', (req, res)=>{
   User.create(req.body)
-      .then(userCreateResponse=> console.log(userCreateResponse))
       .then(()=> res.json({ message: 'user created' }))
       .catch(err => {
-        console.error(err);
         res.status(500).json({ message: JSON.stringify(err) })
       });
 });
 
-
-app.post('/meme', (req, res)=> {
-  // save to db
-  res.status(500).send('no db connection yet');
+app.get('/user/:id', (req, res)=>{
+  User.findByPk(1*req.params.id)
+    .then(user => res.json(user));
 });
 
-app.get('/meme', (req, res)=>{
-  res.json([]);
+app.post('/meme', (req, res)=>{
+  Meme.create(req.body)
+      .then(()=> res.json({ message: 'meme created' }))
+      .catch(err => {
+        res.status(500).json({ message: JSON.stringify(err) })
+      });
 });
+
+app.get('/meme/:id', (req, res)=>{
+  Meme.findByPk(1*req.params.id)
+    .then(meme => res.json(meme));
+});
+
+app.post('/vote', (req, res)=>{
+  Vote.create(req.body)
+      .then(()=> res.json({ message: 'vote created' }))
+      .catch(err => {
+        res.status(500).json({ message: JSON.stringify(err) })
+      });
+});
+
+app.get('/vote/:id', (req, res)=>{
+  Vote.findByPk(1*req.params.id)
+    .then(vote => res.json(vote));
+});
+
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
