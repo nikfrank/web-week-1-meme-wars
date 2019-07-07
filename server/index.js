fdfde8530e26477a434454595253cf94e3bcb7e0
nik frank <nikbfrank@gmail.com>
2019-07-07 03:17:58 -0700

    fake data

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 const ORM = require('sequelize');
-const connection = new ORM('postgres://meme_wars:guest@localhost:5432/meme_wars');
+const connection = new ORM('postgres://memewars:guest@localhost:5432/memewars');
 
 const modelsFactory = require('./models');
 const { User, Meme, Vote } = modelsFactory(connection, ORM);
@@ -19,9 +19,44 @@ connection.authenticate()
 
 
 app.get('/hydrate', (req, res)=> {
+  let users = [
+    { name: 'nik' }, { name: 'avi' }, { name: 'dan' },
+  ];
+
+  let memes = [
+    'http://theawkwardyeti.com/wp-content/uploads/2015/01/0121_Heartwatchesthenews.png',
+    'https://media.discordapp.net/attachments/576550326509764619/589870367968067584/Snapchat-1663181687.jpg?width=725&height=666',
+    'https://cdn.discordapp.com/attachments/576550326509764619/588542078460362753/Snapchat-1743849407.jpg',
+    'https://cdn.discordapp.com/attachments/576550326509764619/587878048087539713/image0.jpg',
+    'https://cdn.discordapp.com/attachments/576550326509764619/593075547815280661/bobbhlkash631.png',
+    'https://images-ext-2.discordapp.net/external/G_e-rxPLhczo-uGCM2W0O85uPyRaZ26AQyv6LCGBdCk/https/bit.ly/2Xd9RnB?width=589&height=619',
+  ];
+
   User.sync({ force: true })
     .then(()=> Meme.sync({ force: true }))
     .then(()=> Vote.sync({ force: true }))
+    .then(()=> User.bulkCreate(users, { returning: true }))
+    .then((userResponse)=> {
+      users = userResponse.map(user => user.dataValues);
+    })
+    .then(()=> memes.map((imgUrl, i)=> ({
+      imgUrl, author: users[i % users.length].id,
+    })))
+    .then((memesWithAuthor)=> Meme.bulkCreate(memesWithAuthor, { returning: true }))
+    .then(memesResponse=> {
+      memes = memesResponse.map(meme => meme.dataValues);
+    })
+    .then(()=>{
+      let votes = [];
+      for(let i=0; i<100; i++){
+        votes.push({
+          winner: memes[Math.floor(Math.random()*memes.length)].id,
+          loser: memes[Math.floor(Math.random()*memes.length)].id,
+          voter: users[i % users.length].id,
+        });
+      }
+      return Vote.bulkCreate(votes, { returning: true });
+    })
     .then(()=> res.json({ message: 'success creating User, Meme, Vote tables' }))
     .catch(err => res.status(500).json({ message: JSON.stringify(err) }));
 });
